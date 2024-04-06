9685aa3ba998a1d4eeaaa322e9d6020a4e2fd167
dacasfilipe <filiperdacas@gmail.com>
2024-04-05 21:55:24 -0300

    implemented CoffeeController.javacontroller

--- src/main/java/br/com/senai/controllers/CoffeeController.java
+++ src/main/java/br/com/senai/controllers/CoffeeController.java
@@ -4,9 +4,7 @@ import br.com.senai.models.Coffee;
 import br.com.senai.repositories.CoffeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,18 @@ public class CoffeeController {
             produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Coffee> getAllCoffee(){
         return coffeeRepository.findAll();
+    }
+
+    @PostMapping(value="/createCoffee",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Coffee createCoffee(@RequestBody Coffee coffee){
+        //Cria um novo objeto Coffee
+        Coffee newCoffee = new Coffee();
+        //Seta as propriedades do Coffee
+        newCoffee.setName(coffee.getName());
+        newCoffee.setPrice(coffee.getPrice());
+        //Chama o método save para salvar o objeto no banco de dados
+        return coffeeRepository.save(newCoffee);
     }
 }
